0998fbf840effa9fddbfa6e7f2654803f86f7b98
Jason Veng <jasonveng@outlook.com>
2018-10-09 13:08:33 +1300

    added news api node library

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
+const NewsAPI = require('newsapi')
 
 const app = express()
 
-
-
+const newsapi = new NewsAPI('API_KEY')
+newsapi.v2
+  .topHeadlines({
+    sources: 'bbc-news',
+    language: 'en'
+  })
+  .then(response => {
+    console.log(response)
+  })
 
 const port = process.env.PORT || 8080
 app.listen(port)
